9f74a68aacab0cdda80f4f2890986ef53df297ae
sniyaz <sniyaz@bk.ru>
2022-10-19 17:15:03 +0300

    Init Home Work Exception

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,123 @@
+package ru.netology
+
+import java.lang.RuntimeException
+
+fun main() {
+    val postOne = Post(1,165002,165003,777,1650508085,"One",77,88,true,
+    Comments(12,true,true,true,true), copyright(12,"","New name", "type"),
+    likes(100,true,true,true), repost(15,true), views(200), "post",
+    geo("Cont", "55.55 49.49"),101,true, false,false,true,true,
+    true, donut(false,1662000, "No dount", true,"All"),
+    false
+    )
+    val postTwo = Post(2,165002,165003,777,1650508085,null,77,88,true,
+        Comments(12,true,true,true,true),copyright(12,"","New name", "type"),
+        likes(100,true,true,true), repost(15,true),views(200), "post",
+        geo("Cont", "55.55 49.49"),101,true, false,false,true,true,
+        true, donut(false,1662000, "No dount", true,"All"),
+        false)
+
+    WallService.add(postOne)
+    WallService.add(postTwo)
+
+    val postId = 2
+
+    println(WallService.posts.size)
+
+    try {
+        WallService.createComment(postId, Comments(12,true, false,true,true))
+        val commetsSize = WallService.comments.size
+        println(commetsSize)
+    } catch (e: RuntimeException){
+        throw PostNotFoundException("Post id $postId not found")
+    }
+}
+
+data class Post(
+    val id: Int = 1,
+    val ownerId: Int = 165002,
+    val fromId: Int = 165003,
+    val createdBy: Int = 777,
+    val date: Long = System.currentTimeMillis() / 1000,
+    val text: String?,
+    val replyOwner_id: Int = 77,
+    val replyPost_id: Int = 88,
+    val friendsOnly: Boolean = true,
+    val comments: Comments,
+    val copyright: copyright,
+    val likes: likes,
+    val repost: repost,
+    val views: views,
+    val postType: String = "post",
+    val geo: geo,
+    val singerId: Int = 101,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = false,
+    val canEdit: Boolean = true,
+    val isPinned: Boolean = true,
+    val markedAsAds: Boolean = true,
+    val isFavorite: Boolean = true,
+    val donut: donut,
+    val postponedId: Boolean = false
+
+)
+
+object WallService {
+
+    var posts = emptyArray<Post>()
+    var comments = emptyArray<Comments>()
+    fun add(post: Post): Post {
+        if (post.text !== null)
+        posts += post
+        return posts.last()
+    }
+    fun createComment(postId: Int, comment: Comments): Comments {
+        for (post in posts){
+            if (post.id == postId){
+                comments += comment
+            }
+        }
+        return comments.last()
+    }
+}
+class PostNotFoundException(message: String) : RuntimeException(message)
+
+class Comments(
+    val count: Int = 12,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = false,
+    val canClose: Boolean = true,
+    val canOpen: Boolean = true
+)
+
+class copyright(
+    val id: Int = 12,
+    val  link: String = "",
+    val name: String = "New name",
+    val type: String = "type"
+)
+class likes(
+    val count: Int = 100,
+    val userLikes: Boolean = true,
+    val canLike: Boolean = true,
+    val canPuplish: Boolean = true
+
+)
+class repost(
+    val count: Int = 15,
+    val userReposted: Boolean = true
+)
+class views(
+    val count: Int = 200
+)
+class geo(
+    val type: String = "Continent",
+    val coordinates: String = "55.966943 49.211736",
+)
+class donut(
+    val isDonut: Boolean = false,
+    val paidDuration: Int = 1662000,
+    val placeholder: String = "No dount",
+    val canPublishFreeCopy: Boolean = true,
+    val editMode: String = "All"
+)
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,44 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.netology.*
+import ru.netology.WallService
+import ru.netology.Comments
+import java.lang.RuntimeException
+
+class WallServiceTest {
+
+    @Test
+    fun createCommentIfPostHave() {
+        val postId: Int = 1
+        val postOne = Post(1,165002,165003,777,1650508085,"One",77,88,true,
+            Comments(12,true,true,true,true), copyright(12,"","New name", "type"),
+            likes(100,true,true,true), repost(15,true), views(200), "post",
+            geo("Cont", "55.55 49.49"),101,true, false,false,true,true,
+            true, donut(false,1662000, "No dount", true,"All"),
+            false
+        )
+        var comments = emptyArray<Comments>()
+        val comment = Comments(12,true,false,true,true)
+            if (postOne.id == postId){
+                comments += comment
+            }
+        val result = comments.size
+
+        assertEquals(1, result)
+
+    }
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        // здесь код с вызовом функции, которая должна выкинуть PostNotFoundException
+        val postId = 2
+
+        try {
+            WallService.createComment(postId, Comments(12,true, false,true,true))
+            val commetsSize = WallService.comments.size
+            println(commetsSize)
+        } catch (e: RuntimeException){
+            throw PostNotFoundException("Post id $postId not found")
+        }
+    }
+}
